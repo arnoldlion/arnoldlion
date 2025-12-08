6259433cdb1859aae0df48b566756cb4f9b30114
igorkuntsevich <igorkuntsevich@gmail.com>
2025-12-08 13:32:57 +0300

    Add Threads

--- src/subitems/social-media/index.js
+++ src/subitems/social-media/index.js
@@ -7,6 +7,7 @@ export const SocialMedia = () => {
       <a href="https://www.facebook.com/groups/734815114264815" target="_blank" className={styles.social_link + " " + styles.social_link_fb}>Facebook</a>
       <a href="https://www.instagram.com/arnoldlioncattery/" target="_blank" className={styles.social_link + " " + styles.social_link_insta}>Instagram</a>
       <a href="https://www.tiktok.com/@arnold.lion" target="_blank" className={styles.social_link + " " + styles.social_link_tiktok}>TikTok</a>
+      <a href="https://www.threads.com/@arnoldlioncattery" target="_blank" className={styles.social_link + " " + styles.social_link_threads}>Threads</a>
     </div>
   );
 }
