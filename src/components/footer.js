66a9633b19cdabf8467a013311dba023301fdfca
igorkuntsevich <igorkuntsevich@gmail.com>
2025-12-08 14:35:19 +0300

    Add WeChat

--- src/components/footer.js
+++ src/components/footer.js
@@ -23,6 +23,7 @@ const Footer = () => {
             <div className="footer_cons_messages">
               <a href="tg://resolve?domain=annamaxilife" className="footer_cons_link footer_cons_link_telegram">Telegram</a>
               <a href="whatsapp://send?phone=+375447401755" className="footer_cons_link footer_cons_link_whatsapp">Whatsapp</a>
+              <a href="https://u.wechat.com/kADy2pzy43KJgNbACOsWqP0?s=2" className="footer_cons_link footer_cons_link_wechat" target="_blank" rel="noreferrer">WeChat</a>
             </div>
           </div>
         </div>
